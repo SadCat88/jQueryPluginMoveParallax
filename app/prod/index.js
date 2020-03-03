7ee7aecba28d0f7e4be9918c8ea7a378f610ba8b
SadCat88 <SadCat88@yandex.ru>
2020-03-03 14:28:26 +0300

    Триганометрия. Расчет положения слоев по оси X в зависимости от удаления от точки нулевой перспективы.

--- app/prod/index.js
+++ app/prod/index.js
@@ -1,12 +1,12 @@
 'use strict';
+
 jQuery.noConflict();
 (($) => {
   $.fn.mouseMoveParallax = function() {
     return this.each(function() {
       // === настройки по умолчанию
       const defaults = {
-        qtyLayers: 100,
-        perspectiveLength: 1000,
+        perspectiveLength: 100,
       };
 
       // === глобальные переменные
@@ -16,12 +16,10 @@ jQuery.noConflict();
       // объект с размерами окна и точкой центра
       let deviationFromCenter = {};
       //  объект с координатами отклонения курсора от центра окна
-      let elementPosition = {};
+      let layerPosition = {};
       // объект хранящий координаты position top/left для перемещаемого объекта
 
       console.log($this);
-
-      // const parallaxItemPosition = ;
 
       // === начальное состояние окна браузера
       // прослушивание события движения мыши
@@ -33,13 +31,12 @@ jQuery.noConflict();
           centerX: $(window).width() / 2,
           centerY: $(window).height() / 2,
         };
-        console.log('parallaxReady -> windowParameters', windowParameters);
 
-        elementPosition = {
+        layerPosition = {
           top: $this.css('top'),
           left: $this.css('left'),
+          depth: $this.data('perspective-depth'),
         };
-        console.log('parallaxReady -> elementPosition', elementPosition);
       }
 
       function getPositionMouse(event) {
@@ -53,10 +50,6 @@ jQuery.noConflict();
           X: mousePosition.X - windowParameters.centerX,
           Y: mousePosition.Y - windowParameters.centerY,
         };
-        console.log(
-          'getPositionMouse -> deviationFromCenter',
-          deviationFromCenter
-        );
       }
 
       // === нарисовать композицию в зависимости от координат курсора
@@ -65,19 +58,45 @@ jQuery.noConflict();
         //   |\
         //   | \
         //   |  \
-        //   |   \
-        //   |____\
-        //   B     C
+        //  D|___\E ← move parallax layer
+        //   |    \
+        //   |_____\ ← mouse deviation from centre
+        //   B      C
 
-        const BC = deviationFromCenter.X;
+        // === перевод градусов в радианы
+        function toRad(deg) {
+          return (Math.PI * deg) / 180;
+        }
+
         const AB = defaults.perspectiveLength;
+        const BC = deviationFromCenter.X;
+
+        // const AB = 100;
+        // const BC = 70;
+
         const AC = Math.sqrt(BC ** 2 + AB ** 2);
-        const angleA = (180 / Math.PI) * Math.atan2(BC, AB);
-        const angleC = 180 - 90 - angleA;
+        console.log('parallaxDraw -> AC', AC);
+        const degA = Math.round((180 / Math.PI) * Math.atan2(BC, AB));
+        console.log('parallaxDraw -> degA', degA);
+        const degC = 180 - 90 - degA;
+        console.log('parallaxDraw -> degC', degC);
+
+        const AD = layerPosition.depth;
+        console.log('parallaxDraw -> AD', AD);
+        const degE = degC;
+        console.log('parallaxDraw -> degE', degE);
+        const radE = toRad(degE);
+        const sinE = Math.sin(radE);
+        const AE = AD / sinE;
+        console.log('parallaxDraw -> AE', AE);
+        const tanE = Math.tan(radE);
+        console.log('parallaxDraw -> tanE', tanE);
+        const DE = AD / tanE;
+        console.log('parallaxDraw -> DE', DE);
 
         $this.css({
-          left: parseInt(elementPosition.left) + deviationFromCenter.X,
-          top: parseInt(elementPosition.top) + deviationFromCenter.Y,
+          left: parseInt(layerPosition.left) + DE,
+          // top: parseInt(layerPosition.top) + deviationFromCenter.Y,
         });
       }
 
@@ -87,7 +106,7 @@ jQuery.noConflict();
         .on('load', function() {
           parallaxReady();
         })
-        .on('mousemove', function(event) {
+        .on('click', function(event) {
           getPositionMouse(event);
           parallaxDraw();
         });
